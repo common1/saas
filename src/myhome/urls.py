59b6c025c381fa81baf99896f055b8690794792f
Harrie Smulders <h.l.w.smulders@tue.nl>
2025-10-22 08:09:06 +0200

    02 Your First HTML Webpage with Python Functions - Part 2

--- src/myhome/urls.py
+++ src/myhome/urls.py
@@ -20,6 +20,9 @@ from django.urls import path
 from .views import home_page_view
 
 urlpatterns = [
+    path("", home_page_view), # index page -> root page
     path("hello-world/", home_page_view),
+    path("hello-world.html", home_page_view),
     path('admin/', admin.site.urls),
 ]
+
